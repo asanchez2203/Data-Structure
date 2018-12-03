fe7f88610090024ec007b6262189476745a46b2c
Alexis Sánchez <Alexis Snchez@DESKTOP-94KQ5O7>
2018-12-02 18:23:22 -0600

    Ejercicio 30 terminado

--- src/Ejercicio30/IdyLista.java
+++ src/Ejercicio30/IdyLista.java
@@ -1,45 +1,26 @@
 package Ejercicio30;
-/**
- * Clase para almacenar una pareja constituida por un nombre y una lista
- * asociada a �l.
- * @author Amparo L�pez Gaona
- * @version 1.
- */
+
 class IdyLista {
-  public String nombre;
-  public Lista lista;
 
-  /** Constructor que recibe una cadena
-   * @param n - Cadena que ser� el nombre de la lista
-   */
-  public IdyLista (String n)     {
-    nombre = n;
-    lista = new Lista();
-  }
+    public String nombre;
+    public Lista lista;
 
-  /** Constructor que recibe una cadena y una lista
-   * @param n - Cadena que ser� el nombre de la lista
-   * @param l - Lista asociada al nombre dado
-   */
-  public IdyLista (String n, Lista l)     {
-    nombre=n;
-    lista=l;
-  }
-
-  /**
-   * M�todo para obtener el nombre del objeto
-   * @String nombre asociado al objeto
-   */
-    public String obtenerNombre() {
-	return nombre;
+    public IdyLista(String n) {
+        nombre = n;
+        lista = new Lista();
     }
 
-  /**
-   * M�todo para obtener la lista del objeto
-   * @Lista -- lista del objeto
-   */
+    public IdyLista(String n, Lista l) {
+        nombre = n;
+        lista = l;
+    }
+
+    public String obtenerNombre() {
+        return nombre;
+    }
+
     public Lista obtenerLista() {
-	return lista;
+        return lista;
     }
 
 }
--- src/Ejercicio30/ListaPalabrasReservadas.java
+++ src/Ejercicio30/ListaPalabrasReservadas.java
@@ -2,23 +2,40 @@ package Ejercicio30;
 
 import java.io.StreamTokenizer;
 import java.io.*;
+import java.util.Scanner;
 
 public class ListaPalabrasReservadas {
+
+    String[] palabras;
+    String[] lenguajes={"Java.txt","C#.txt","c++.txt","Python.txt"};
+    int[] size={50,76,73,31};
+    int opcion;
+    String filepath,seguir,opc;
+    FileReader r;
+    BufferedReader br;
+    ListaDeIds lisId;
+    Scanner sc=new Scanner(System.in);
+
+    void out(String s) { System.out.println(s);}
+    void err(String s) { System.err.println(s);}
     
-    public static boolean palRes(String palabra) {
-        String[] palabras = {"abstract", "else", "interface", "static",
-            "boolean", "extends", "long", "super",
-            "break", "false", "main", "switch",
-            "byte", "final", "native", "synchronized",
-            "case", "finally", "new", "this",
-            "catch", "float", "null", "throw",
-            "char", "for", "package", "throws",
-            "class", "if", "private", "true",
-            "continue", "implements", "protected", "try",
-            "default", "import", "public", "void",
-            "do", "instanceof", "return", "volatile",
-            "double", "int", "short", "while"};
-        
+    void meta(){
+        out("Escriba un programa que pida al usuario la ruta de un archivo");
+        out("de texto o codigo fuente para contabilizar y ubicar las palabras");
+        out("no reservadas de un lenguaje de progrmacion");
+    }
+
+    void leerRutaArchivo() {
+        boolean existe=false;
+        do{
+            out("Digite la ruta de su archivo");
+            filepath=sc.nextLine();
+            if(new File(filepath).exists()) existe=true;
+            else err("El archivo no existe");
+        }while(!existe);
+    }
+
+    boolean palRes(String palabra) {
         for (int i = 0; i < palabras.length; i++) {
             if (palabras[i].equals(palabra)) {
                 return true;
@@ -26,37 +43,89 @@ public class ListaPalabrasReservadas {
         }
         return false;
     }
-    
-    public static void leerPalReservadas(String s) {
-    }
-    
-    public static void main(String[] pps) {
-        ListaDeIds lisId = new ListaDeIds();
+
+    void ComparaPalabras() {
+        lisId = new ListaDeIds();
         int tipoTok;
-//
-//        if (pps.length != 2) {
-//            System.out.println("Uso: ListaPalabrasReservadas archivoFuente");
-//            System.exit(0);
-//        }
-//        leerPalReservadas(pps[0]);
+
         try {
-            BufferedReader in = new BufferedReader(new FileReader("C:\\Users\\Alexis Sánchez\\Documents\\NetBeansProjects\\Data-Structure\\src\\Ejercicio1\\Main.java"));
+            BufferedReader in = new BufferedReader(new FileReader(filepath));
             StreamTokenizer tok = new StreamTokenizer(in);
             tok.ordinaryChar('.');
             tipoTok = tok.nextToken();
-            
+
             while (tipoTok != StreamTokenizer.TT_EOF) {
-                if (tipoTok == StreamTokenizer.TT_WORD) {
-                    if (palRes(tok.sval)) {
-                        lisId.insertar(tok.sval, new Integer(tok.lineno()));
-                    }
-                }
+                if (tipoTok == StreamTokenizer.TT_WORD) 
+                    if (!palRes(tok.sval)) 
+                        lisId.insertar(tok.sval, tok.lineno());     
                 tipoTok = tok.nextToken();
             }
-            
-            lisId.imprimir();
         } catch (IOException e) {
             System.out.println("Se genero IOException " + e);
         }
     }
+    
+    void imprimeResultados(){ lisId.imprimir();}
+    
+    void openInFile(String s) {
+        try {
+            r = new FileReader(s);
+            br = new BufferedReader(r);
+        } catch (FileNotFoundException ex) {
+            err("File not found");
+        }
+    }
+    
+    void palabrasReservadas(int i){
+        openInFile(lenguajes[i]);
+        palabras=new String[size[i]];
+        for (int j = 0; j < size[i]; j++)
+            try {
+                palabras[j]=br.readLine();
+            } catch (IOException ex) {
+                err("IO Excpetion");
+            }
+    }
+    
+    boolean isNum(String s){
+        try { 
+            int opcion;
+            opcion=Integer.parseInt(s);
+            if(opcion<1&&opcion>4){
+                err("Opcion inválida");
+                return false;
+            }
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+    
+    void navegabilidad(){
+        do{
+            out("Escoja su lenguaje para comparar su archivo");
+            out("1.- JAVA");
+            out("2.- C#");
+            out("3.- C++");
+            out("4.- PYTHON");
+            out("Su elección: ");
+            do opc=sc.nextLine(); while(!isNum(opc));
+            opcion=Integer.parseInt(opc);
+            palabrasReservadas(opcion-1);
+            
+            leerRutaArchivo();
+            ComparaPalabras();
+            imprimeResultados();
+            
+            out("Desea hacerlo de nuevo? s/n");
+            seguir=sc.nextLine();
+        }while(seguir.equalsIgnoreCase("s"));
+    }
+    
+    public static void main(String[] args) {
+        ListaPalabrasReservadas ls=new ListaPalabrasReservadas();
+        ls.meta();
+        ls.navegabilidad();
+    }
+
 }
